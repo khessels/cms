398b0680e727233618903de95c9d40d12ad32595
khessels <kees.hessels@gmail.com>
2026-04-13 15:04:33 -0600

    bf cmsRoutes()

--- routes/web.php
+++ routes/web.php
@@ -4,39 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Khessels\Cms\Controllers\ContentController;
 use Khessels\Cms\Controllers\LanguageController;
 
-if(config('cms.enabled')) {
-    if( config('cms.route_debug')) error_log('CMS enabled');
-    if ( sizeof( explode( ',', config('cms.route_middleware_encapsulation'))) > 0) // default web, language
-    {
-        if( config('cms.route_debug')) error_log('CMS route_middleware_encapsulation: ' . config('cms.route_middleware_encapsulation'));
-        Route::group(['middleware' => explode(',', config('cms.route_middleware_encapsulation'))], function () {
-            Route::group(['prefix' => 'language'], function () {
-                Route::post('/switch', [LanguageController::class, 'update'])->name('post.language.switch');
-            });
-            if ( ! empty( config('cms.spatie_permission'))) {
-                if( config('cms.route_debug')) error_log('CMS spatie_permission: ' . config('cms.spatie_permission'));
-                Route::group(['middleware' => ['permission:' . strtolower(config('cms.spatie_permission'))]], function () {
-                    cmsRoutes();
-                });
-            } else {
-                cmsRoutes();
-            }
-        });
-    } else {
-        if( config('cms.route_debug')) error_log('CMS NO route_middleware_encapsulation');
-        Route::group(['prefix' => 'language'], function () {
-            Route::post('/switch', [LanguageController::class, 'update'])->name('post.language.switch');
-        });
-        if (!empty(config('cms.spatie_permission'))) {
-            Route::group(['middleware' => ['permission:' . strtolower(config('cms.spatie_permission'))]], function () {
-                cmsRoutes();
-            });
-        } else {
-            cmsRoutes();
-        }
-
-    }
-}
 function cmsRoutes(): void
 {
     Route::group(['prefix' => config('cms.route_prefix')], function () {
@@ -88,4 +55,38 @@ function cmsRoutes(): void
     });
     //Route::get('/cms/image/data', [ContentController::class, 'getImageData'])->name('cms.image.data.get');
     Route::fallback([ContentController::class, 'getPageFromCMS']);
+}
+
+if(config('cms.enabled')) {
+    if( config('cms.route_debug')) error_log('CMS enabled');
+    if ( sizeof( explode( ',', config('cms.route_middleware_encapsulation'))) > 0) // default web, language
+    {
+        if( config('cms.route_debug')) error_log('CMS route_middleware_encapsulation: ' . config('cms.route_middleware_encapsulation'));
+        Route::group(['middleware' => explode(',', config('cms.route_middleware_encapsulation'))], function () {
+            Route::group(['prefix' => 'language'], function () {
+                Route::post('/switch', [LanguageController::class, 'update'])->name('post.language.switch');
+            });
+            if ( ! empty( config('cms.spatie_permission'))) {
+                if( config('cms.route_debug')) error_log('CMS spatie_permission: ' . config('cms.spatie_permission'));
+                Route::group(['middleware' => ['permission:' . strtolower(config('cms.spatie_permission'))]], function () {
+                    cmsRoutes();
+                });
+            } else {
+                cmsRoutes();
+            }
+        });
+    } else {
+        if( config('cms.route_debug')) error_log('CMS NO route_middleware_encapsulation');
+        Route::group(['prefix' => 'language'], function () {
+            Route::post('/switch', [LanguageController::class, 'update'])->name('post.language.switch');
+        });
+        if (!empty(config('cms.spatie_permission'))) {
+            Route::group(['middleware' => ['permission:' . strtolower(config('cms.spatie_permission'))]], function () {
+                cmsRoutes();
+            });
+        } else {
+            cmsRoutes();
+        }
+
+    }
 }
